dc94bb8e1b34984226674b402e25732e594c1cee
Nivaga <79633946+Nivaga37@users.noreply.github.com>
2024-10-29 14:33:37 +0400

    Update список задач.py

--- список задач.py
+++ список задач.py
@@ -1,34 +1,35 @@
 
 tasks=[] # пустой массив можно сделать сохранение в файл
 
-while True:   # бесконечные вопросы
-    def greet(anser):  
-       anser=input("что ты хочешь?\n1-добавить задачу\n2-посмотреть список\n3-удалить задачу")
-       if anser == 1:
-            new_task()
-       elif anser == 2:
-             view_task()
-       elif anser == 3:
-            del_task()
-       else:
-       print ("ты че дурак чтоль?")
-    break
+def new_task(tasks):   # новая штука
+    newtask = input("Введите задачу")
+    tasks.append(newtask)
 
-   
-def new_task():   # новая штука
-new_task=input("Введите задачу")
-list.append(new_task)
+def view_task(tasks): # показать все штуки
+    for i in tasks:
+        print(i) # можно добавить иф при пустом списке
 
-
-def view_task(): # показать все штуки
-    print 
-
-
-def del_task(): # удалить штуку по ее счету или индексу 
+def del_task(tasks): # удалить штуку по ее счету или индексу
 # должно появляться с номерами нужно выводить numder с index(number)
-    list.count()
-    
-    for i in range()
-    
-    list.pop
-    
+    number = 1
+    for i in tasks:
+        print(number,i)
+        number = number + 1
+    deltask = int(input("какую номер задачи удалить?"))
+    deltask = deltask - 1 # гребаные индексы
+    if deltask <= number:
+        tasks.pop(deltask)
+    else:
+        print ("ты че дурак чтоль?") 
+        return
+
+while True:   # бесконечные вопросы?????
+    anser = int(input("что ты хочешь?\n1-добавить задачу\n2-посмотреть список\n3-удалить задачу\n"))
+    if anser == 1:
+        new_task(tasks)
+    elif anser == 2:
+        view_task(tasks)
+    elif anser == 3:
+        del_task(tasks)
+    else:
+        print ("ты че дурак чтоль?")
